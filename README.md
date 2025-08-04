5a80f10450a104aec9e9fe3506b87ad1b6436968
TheCyberHawk <himanshuvats135790@gmail.com>
2025-08-04 13:59:38 +0530

    Update and rename README.md to README.py

--- README.md
+++
@@ -1,2 +0,0 @@
-# SQL-Injection-Finder
-SQL Injection Finder-Tool using Python: Custom tool to detect SQL injection vulnerabilities in web apps.
---
+++ README.py
@@ -0,0 +1,18 @@
+import subprocess
+
+url = input("enter your url: ")
+
+subprocess.call(["sqlmap", "-u", url , "--dbs", "--batch"])
+
+database = input("enter your databse name: ")
+
+subprocess.call(["sqlmap", "-u", url, "-D", database, "--tables", "--batch"])
+
+tables_name = input("enter your tables name: ")
+
+subprocess.call(["sqlmap", "-u", url, "-D", database, "-T", tables_name , "--columns", "--batch"])
+
+columns_name = input("enter your coloums name: ")
+
+subprocess.call(["sqlmap", "-u", url, "-D", database, "-T", tables_name, "-C", columns_name, "--dump", "--batch"])
+sqlmap.py          
